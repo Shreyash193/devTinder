311181b224c6acad2623a0df7c05bf92a47580c0
Shreyash Gore <shreyashgore193@gmail.com>
2025-01-01 11:52:07 +0530

    created login api

--- src/app.js
+++ src/app.js
@@ -34,6 +34,31 @@ app.post("/signup",async (req,res)=>{
   }
 });
 
+app.post("/login",async (req,res)=>{
+  try{
+    const {emailId,password}=req.body;
+
+    const user=await User.findOne({emailId:emailId});
+    if(!user){
+      throw new Error("invalid crediantials");
+    }
+    
+    const isPasswordValid=await bcrypt.compare(password,user.password);
+
+    if(isPasswordValid){
+      res.send("login successfull!!")
+    }
+    else{
+      res.status(400).send("invalid crediantials");
+    }
+
+  }
+  catch(err){
+    res.status(400).send("ERROR : "+ err.message);
+  }
+  
+})
+
 app.get("/user",async (req,res)=>{
   const userEmail=req.body.emailId;
 
